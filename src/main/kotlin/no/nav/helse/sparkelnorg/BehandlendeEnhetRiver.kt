2a1d4b3a34f12cc558e185ace3795d32dc114467
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-04-07 17:17:45 +0200

    Forbedrer logging

--- src/main/kotlin/no/nav/helse/sparkelnorg/BehandlendeEnhetRiver.kt
+++ src/main/kotlin/no/nav/helse/sparkelnorg/BehandlendeEnhetRiver.kt
@@ -3,14 +3,18 @@ package no.nav.helse.sparkelnorg
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @KtorExperimentalAPI
 class BehandlendeEnhetRiver(
     rapidsConnection: RapidsConnection,
     private val personinfoService: PersoninfoService
 ) : River.PacketListener {
+    private val sikkerLogg: Logger = LoggerFactory.getLogger("tjenestekall")
     init {
         River(rapidsConnection).apply {
             validate { it.requireAny("@behov", listOf("HentEnhet")) }
@@ -24,5 +28,9 @@ class BehandlendeEnhetRiver(
             "HentEnhet" to enhet
         )
         context.send(packet.toJson())
+    }
+
+    override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
+        sikkerLogg.info(problems.toExtendedReport())
     }
 }
--- src/main/kotlin/no/nav/helse/sparkelnorg/HentNavnRiver.kt
+++ src/main/kotlin/no/nav/helse/sparkelnorg/HentNavnRiver.kt
@@ -2,24 +2,33 @@ package no.nav.helse.sparkelnorg
 
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.runBlocking
+import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @KtorExperimentalAPI
 class HentNavnRiver(
     rapidsConnection: RapidsConnection,
     private val personinfoService: PersoninfoService
 ) : River.PacketListener {
+    private val log: Logger = LoggerFactory.getLogger("hent-navn")
+    private val sikkerLogg: Logger = LoggerFactory.getLogger("tjenestekall")
+
     init {
         River(rapidsConnection).apply {
             validate { it.requireAny("@behov", listOf("HentEnhet")) }
-            validate { it.requireKey("fødselsnummer") }
+            validate { it.requireKey("fødselsnummer", "spleisBehovId") }
         }.register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) = runBlocking {
-        val person = personinfoService.finnPerson(packet["fødselsnummer"].asText()) ?: return@runBlocking
+        val fnr = packet["fødselsnummer"].asText()
+        log.info("Henter personinfo for {}", keyValue("spleisBehovId", packet["spleisBehovId"].asText()))
+        val person = personinfoService.finnPerson(fnr) ?: return@runBlocking
 
         packet["@løsning"] = mapOf(
             "HentNavn" to mapOf(
@@ -29,5 +38,9 @@ class HentNavnRiver(
             )
         )
         context.send(packet.toJson())
+    }
+
+    override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
+        sikkerLogg.info(problems.toExtendedReport())
     }
 }
