fba2d26ab22237a126c7bb130f660c2572f9f3b9
Martin Solheim <martin.solheim@me.com>
2020-07-10 12:03:06 +0200

    Bumper dependencies og gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val junitJupiterVersion = "5.5.2"
+val junitJupiterVersion = "5.6.2"
 val ktorVersion = "1.3.2"
-val wireMockVersion = "2.26.0"
-val cxfVersion = "3.3.5"
+val cxfVersion = "3.3.7"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -11,8 +8,15 @@ plugins {
 
 group = "no.nav.helse"
 
+repositories {
+    mavenCentral()
+    maven("https://kotlin.bintray.com/ktor")
+    maven("https://jitpack.io")
+    maven("https://packages.confluent.io/maven/")
+}
+
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.74ae9cb")
+    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
 
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
@@ -28,7 +32,7 @@ dependencies {
     implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
     implementation("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
     implementation("com.sun.activation:javax.activation:1.2.0")
-    implementation("com.sun.xml.ws:jaxws-tools:2.3.1") {
+    implementation("com.sun.xml.ws:jaxws-tools:2.3.3") {
         exclude(group = "com.sun.xml.ws", module = "policy")
     }
 
@@ -36,55 +40,41 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
-    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("io.mockk:mockk:1.10.0")
 }
 
-repositories {
-    mavenCentral()
-    maven("https://kotlin.bintray.com/ktor")
-    maven("https://jitpack.io")
-    maven("https://packages.confluent.io/maven/")
-}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_12
-    targetCompatibility = JavaVersion.VERSION_12
-}
+    named<Jar>("jar") {
+        archiveBaseName.set("app")
 
-tasks.named<Jar>("jar") {
-    archiveBaseName.set("app")
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.sparkelnorg.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
 
-    manifest {
-        attributes["Main-Class"] = "no.nav.helse.sparkelnorg.AppKt"
-        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
-            it.name
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
         }
     }
 
-    doLast {
-        configurations.runtimeClasspath.get().forEach {
-            val file = File("$buildDir/libs/${it.name}")
-            if (!file.exists())
-                it.copyTo(file)
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
         }
     }
 }
 
-tasks.named<KotlinCompile>("compileKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "6.0.1"
-}
